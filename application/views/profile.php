80f63bdb3021099406945de3c52ce3d496200df0
waterol <liam@canvol.org>
2014-04-13 16:32:33 -0600

    fixed color

--- application/views/profile.php
+++ application/views/profile.php
@@ -6,7 +6,7 @@
           </div>
           <div id="profilebiocontainer">
             <h2> Liam Waterous </h2>
-            <p> fjakl;jf;alksfjsdl;kfjkflkasufjalksdjf </p>
+            <p> American Apparel plaid pour-over, chambray flexitarian single-origin coffee meh umami squid biodiesel. Mlkshk authentic Shoreditch, drinking vinegar slow-carb artisan church-key lomo craft beer hashtag Marfa. Plaid High Life hella, leggings American Apparel ethical literally. Church-key selfies pork belly XOXO, blog messenger bag forage pug plaid pop-up. Pug aesthetic butcher, dreamcatcher small batch selvage artisan Tonx literally Truffaut mixtape mustache photo booth. Farm-to-table authentic iPhone, hella cliche chillwave slow-carb post-ironic plaid banh mi fap vegan. Neutra four loko typewriter locavore. </p>
           </div>
           <div id="collage">
 

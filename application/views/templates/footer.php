6a8c5691182fd2119d21bb4d0cd23c2713aaa7c9
Sean Feil <sean@com.tophatandmonocle>
2014-04-13 13:23:48 -0600

    Added a footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,10 @@
+    
+    <div class="container" id="footer">
+      <p>Copyright &copy; 2014 CanVol</p>
+      <p>Privacy Policy | Contact Us</p>
+
+    </div>
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->

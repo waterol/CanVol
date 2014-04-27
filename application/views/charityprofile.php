3482d744b7a19b7a225404a978df14057dc5ab2c
waterol <liam@canvol.org>
2014-04-27 15:50:59 -0600

    Worked on Charity Page

---
+++ application/views/charityprofile.php
@@ -0,0 +1,96 @@
+<div ckass="container">
+	<div id="charityinfo">
+		<div class="row">
+			<div class="col-sm-4">
+				<div id="charitylogo">
+					<img id="charityimage" src="img/fox.png">
+
+				</div>
+				<div id="charitybio">
+				Cliche Marfa Neutra proident aliquip. Tumblr enim hella Godard vinyl magna. Quis Tonx banjo, jean shorts narwhal YOLO cupidatat. XOXO nesciunt banh mi accusamus delectus, id plaid Godard retro brunch. Seitan odio flexitarian meggings Pinterest eu cupidatat cillum. Mustache McSweeney's ethnic, yr authentic reprehenderit blog fashion axe do nihil qui occupy squid proident. Ut selvage kale chips bespoke nulla, keytar master cleanse seitan tattooed labore
+
+				</div>
+
+			</div>
+
+			<div class="col-sm-4">
+
+				<div id="nameofcharity">
+					Michael J. Fox
+
+
+				</div>
+				<div id="contactinformation">
+					<table id="tableinformation">
+						<thead>
+
+
+						</thead>
+						<tbody>
+							<tr>
+								<td>
+
+									link
+								</td>
+								<td>
+									<a href="www.michaeljfoxfoundation.com"> www.michaeljfoxjoundation.com
+									</a>
+
+								</td>
+
+							</tr>
+							<tr>
+								<td>
+									email
+								</td>
+								<td>
+
+									michael@michaeljfox.com
+								</td>
+							</tr>
+							<tr>
+								<td>
+									phone number
+								</td>
+								<td>
+
+									(403) 867-5309
+								</td>
+
+							</tr>
+							<tr>
+								<td>
+								adress
+								</td>
+
+								<td>
+									1111 Fake Street
+
+								</td>
+							</tr>
+
+						</tbody>
+
+					</table>
+
+				</div>
+
+			</div>
+
+
+			<div class="col-sm-4">
+				<div id="percentagecharity">
+
+					68%
+
+				</div>
+
+			</div>
+		</div>
+
+	</div>
+
+
+
+
+</div>

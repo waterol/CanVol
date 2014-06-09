6e3114374b89d681c8ef7d1acba5a545120f9908
waterol <liam@canvol.org>
2014-06-08 20:21:46 -0600

    MySQL

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
+$db['default']['username'] = 'admin';
 $db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['database'] = 'canvol';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;

68cbe06a7a6c3bb98229887b5a7e79282d371a66
waterol <liam@canvol.org>
2014-08-09 16:58:05 -0600

    The Success Page

--- application/views/signup_success.php
+++ application/views/signup_success.php
@@ -1,1 +1,30 @@
-Signup Success
+<div id="signupsuccess">
+
+	<button onclick="location.href='<?=base_url() ?>browsecharities'" id="browseinsuccess" class="btn">
+		Browse Charities
+	</button>
+
+	<div id="successinsuccess">
+		<p id="congratsinsuccess">
+			Congratulations!
+		</p>
+	<button onclick="location.href='<?=base_url() ?>home'" id="homeinsuccess" class="btn">
+		Home Page
+	</button>
+		
+
+		<p id="certifiedinsucess">
+			You are now a certified CanVoler.
+		</p>
+		<p id="browsingcanvolinsuccess">
+		While you are waiting for your activation email, feel free to browse our website.
+		</p>
+	</div>
+
+	
+
+
+
+
+
+</div>

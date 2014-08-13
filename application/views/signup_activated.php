598cf07346ffec3419276c43b0f62362a80733be
waterol <liam@canvol.org>
2014-08-12 19:04:16 -0600

    Completed the Activation Page

--- application/views/signup_activated.php
+++ application/views/signup_activated.php
@@ -1,1 +1,23 @@
-Account Activated
+<div id="activatedpage">
+
+	<div>
+
+	<button onclick="location.href='<?=base_url() ?>profile'" id="activatedonprofile" class="btn">
+		Your Profile Page
+	</button>
+		
+		<p id="activatedtext">
+			You are now Activated!
+		</p>
+
+		
+		<p id="detailsofactivated">
+			Now that you have a profile, you can share your experiences volunteering,
+			<br>
+			post photos and most importantly rate and review charities.
+		</p>
+
+	</div>
+
+
+</div>

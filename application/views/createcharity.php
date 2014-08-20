031f4c5cb2dfdaf1a485bf40898933c629ec09ba
waterol <liam@canvol.org>
2014-08-19 21:19:56 -0600

    Create Charity Page

---
+++ application/views/createcharity.php
@@ -0,0 +1,20 @@
+<div id="bigdivincreatecharity">
+
+	<div id="createcharityeasy">
+		It's easy to create a charity...
+	</div>
+
+
+		<label for="charityname" class=" control-label" id="charitynameincreate"> Charity Name</label>
+		<div>
+			<input type="text" class="form-control signupinput" id="charitynameincreate" name="charityname" placeholder="Charity Name">
+		
+		</div>
+	
+
+	<div class="form-group">
+		<button type="submit" class="btn btn-primary" id="completeregistrationincreate">Complete Registration</button>
+	</div>
+
+
+</div>

c6f06c41214823f0a4bcb91647b8903b71255fdb
waterol <liam@canvol.org>
2014-09-10 20:58:55 -0600

    Browse charities

--- application/views/browsecharities.php
+++ application/views/browsecharities.php
@@ -41,11 +41,11 @@
 		<div class="browsecharitiescharities">
 			<div class="row">
 
-				<div class="col-sm-2">
+				<div class="col-xs-6 col-md-2">
 					<img src="img/profilepicforfox.jpg" class="charitylistimage">
 				</div>
 
-				<div class="col-sm-10">
+				<div class="col-xs-6 col-md-10">
 					<p class="titleinbrowsecharities"><?=$charity['name'] ?></p>
 					<p class="addressinbrowsecharities">
 						Address: <?=$charity['location'] ?>
